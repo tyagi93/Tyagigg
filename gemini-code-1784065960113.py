6ff96430b38e4641d710ed8cf510fdd8534b82b0
tyagi93 <himanshuvatstyagi013@gmail.com>
2026-07-15 03:43:59 +0530

    Update gemini-code-1784065960113.py

--- gemini-code-1784065960113.py
+++ gemini-code-1784065960113.py
@@ -1,5 +1,5 @@
 BOT_TOKEN = "7243707544:AAGgPrkHFMBlPNgmyLvM_jKxH9AL_eDJ8Ik"  # Yahan apna Telegram Bot Token dalein
-ADMIN_ID = 123456789                  # Yahan apni numeric Telegram ID dalein
+ADMIN_ID = 5744767539                 # Yahan apni numeric Telegram ID dalein
 API_URL = "https://number-to-info-s0ry.onrender.com/lookup/"
-BOT_NAME = "Number Info Bot"
-SUPPORT = "@YourAdminUsername"          # Yahan apna Telegram username dalein
+BOT_NAME = "TYAGI Number Info Bot"
+SUPPORT = "@Tyagibhai08"          # Yahan apna Telegram username dalein
