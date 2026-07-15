b3b3895e1f05eaee8c94766e89116e4f120e5bd9
tyagi93 <himanshuvatstyagi013@gmail.com>
2026-07-15 07:56:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ BOT_TOKEN = "7243707544:AAGgPrkHFMBlPNgmyLvM_jKxH9AL_eDJ8Ik"  # Yahan apna Teleg
 ADMIN_ID = "5744767539"                # Yahan apni numeric Telegram ID dalein
 API_URL = "https://number-to-info-s0ry.onrender.com/lookup/"
 BOT_NAME = "Number To Info Bot "
-SUPPORT = "@Tyagibhai08"          # Yahan apna Telegram username dalein
+SUPPORT = "@TYAGI8"          # Yahan apna Telegram username dalein
