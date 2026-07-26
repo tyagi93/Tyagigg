fe0b0667bab9e8abe52411120fd2a2057d406970
tyagi93 <himanshuvatstyagi013@gmail.com>
2026-07-26 16:56:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 BOT_TOKEN = "8996186987:AAFeF_T7tdfcHXRN-_0OwlDBmxCuKsqgpiM"  # Yahan apna Telegram Bot Token dalein
 ADMIN_ID = "5744767539"                # Yahan apni numeric Telegram ID dalein
-API_URL = "https://number-to-info-s0ry.onrender.com/lookup/"
+API_URL = "https://rapidapi.com/lookup/"
 BOT_NAME = "TYAGI Number To Info Bot "
 SUPPORT = "@TYAGI8"          # Yahan apna Telegram username dalein
 VEHICLE_API_URL = "https://vehicleinfo-byrack.vercel.app/api?search="
