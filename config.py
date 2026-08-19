2d758dc6deaf0ce2f7fae41ff7a01b05bca2178e
tyagi93 <himanshuvatstyagi013@gmail.com>
2026-08-20 04:49:26 +0530

    Update and rename config.py to music_bot.py

--- config.py
+++
@@ -1,7 +0,0 @@
-BOT_TOKEN = "8996186987:AAFeF_T7tdfcHXRN-_0OwlDBmxCuKsqgpiM"
-ADMIN_ID = "5744767539"
-API_URL = "http://subhxcosmo.in"
-BOT_NAME = "TYAGI Number To Info Bot "
-SUPPORT = "@TYAGI8"
-VEHICLE_API_URL = "https://vehicleinfo-byrack.vercel.app/api?search="
-#
