2c7ae584f5a808a2be136d1a67c8f613e32241f6
tyagi93 <himanshuvatstyagi013@gmail.com>
2026-08-20 01:40:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,6 @@
 BOT_TOKEN = "8996186987:AAFeF_T7tdfcHXRN-_0OwlDBmxCuKsqgpiM"  # Yahan apna Telegram Bot Token dalein
 ADMIN_ID = "5744767539"                # Yahan apni numeric Telegram ID dalein
+API_URL = "https://rapidapi.com/lookup/"
 BOT_NAME = "TYAGI Number To Info Bot "
 SUPPORT = "@TYAGI8"          # Yahan apna Telegram username dalein
-
-# 🚗 Vehicle API pehle jaisi hi hai, isko bilkul nahi badla
 VEHICLE_API_URL = "https://vehicleinfo-byrack.vercel.app/api?search="
-
-# 📞 Sirf Number to Info API ko naye format mein badla gaya hai
-BASE_URL = "http://subhxcosmo.in"
-API_KEY = "ITACHI"
-API_URL = f"{BASE_URL}?api_key={API_KEY
