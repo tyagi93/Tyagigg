735c4494a7c560c184837cdd95e977adf5bf9df6
tyagi93 <himanshuvatstyagi013@gmail.com>
2026-08-20 04:33:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-BOT_TOKEN = "8996186987:AAFeF_T7tdfcHXRN-_0OwlDBmxCuKsqgpiM"  # Telegram Bot Token
-ADMIN_ID = "5744767539"                # Numeric Telegram ID
-API_URL = "http://subhxcosmo.in" # Purana working link parameter ke sath
+BOT_TOKEN = "8996186987:AAFeF_T7tdfcHXRN-_0OwlDBmxCuKsqgpiM"
+ADMIN_ID = "5744767539"
+API_URL = "http://subhxcosmo.in"
 BOT_NAME = "TYAGI Number To Info Bot "
-SUPPORT = "@TYAGI8"          # Telegram username
+SUPPORT = "@TYAGI8"
 VEHICLE_API_URL = "https://vehicleinfo-byrack.vercel.app/api?search="
+#
