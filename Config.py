458e9d4d9c30f673ebfde6d3a6f1d3899b4030e0
tyagi93 <himanshuvatstyagi013@gmail.com>
2026-09-01 08:28:36 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,8 @@
+BOT_TOKEN = "7243707544:AAGgPrkHFMBlPNgmyLvM_jKxH9AL_eDJ8Ik"  # Telegram Bot Token
+ADMIN_ID = "5744767539"                # Admin numeric Telegram ID
+BOT_NAME = "Number & Vehicle Info Bot"
+SUPPORT = "@Tyagibhai08"                # Support username
+
+# Naye API Endpoints
+NUMBER_API_URL = "https://workers.dev"
+VEHICLE_API_URL = "https://vercel.app"
